656075c113af569ee6db0b8178e4f7a474fc7cd2
zayyo123 <764439554@qq.com>
2023-05-09 21:45:06 +0800

    后台管理系统

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,11 @@ import { uploadCache } from "@/store";
 // 在不同的浏览器中为HTML元素提供一致的默认样式。
 import "normalize.css";
 
+// 导入自定义的基础样式和阿里巴巴图标库的 CSS 文件
 import "./assets/css/index.less";
+// 导入我们Element plush的组件自动注册函数
 import register from "./global";
+//导入自定义的浏览器本地缓存对象
 import localCache from "@/utils/cache";
 
 // 缓存登录密码
