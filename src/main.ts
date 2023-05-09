90dd27bdc79433472895000162797c8bec55ff2e
zayyo123 <764439554@qq.com>
2023-05-09 21:40:44 +0800

    后台管理系统

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { uploadCache } from "@/store";
+// 在不同的浏览器中为HTML元素提供一致的默认样式。
 import "normalize.css";
 
 import "./assets/css/index.less";
 import register from "./global";
 import localCache from "@/utils/cache";
 
+// 缓存登录密码
 localCache.setCache("name", "zayyo");
 localCache.setCache("password", "123456");
 // 使用
